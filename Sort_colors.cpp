68651fa66a9348d51ea640f20b3dc5f95647a369
Mangesh Jaybhaye <132568893+MangeshJ10603@users.noreply.github.com>
2024-07-16 23:14:52 +0530

    Sort Colors 👍

--- Sort_colors.cpp
+++ Sort_colors.cpp
@@ -3,7 +3,7 @@ public:
     void sortColors(vector<int>& nums) {
         int start = 0;
         int end=nums.size()-1;
-        int curr=0;
+        int curr=0; 
         while(curr<=end){
             if(nums[curr]==0){
                 swap(nums[curr], nums[start]);
